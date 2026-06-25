cfedf45ba68d0db0e370c7dcd8d89a2787bb7581
Sunny <cbe112030@nptu.edu.tw>
2026-06-25 17:09:12 +0800

    add App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Cosmetics from "./pages/Cosmetics";
@@ -9,40 +9,14 @@ import Skincare from "./pages/Skincare";
 
 function App() {
   return (
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route
-          path="/cosmetics"
-          element={<Cosmetics />}
-        />
-
-        <Route
-          path="/skincare"
-          element={<Skincare />}
-        />
-
-        <Route
-          path="/palette"
-          element={<Palette />}
-        />
-
-        <Route
-          path="/shopping"
-          element={<Shopping />}
-        />
-
-        <Route
-          path="/expiry"
-          element={<Expiry />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cosmetics" element={<Cosmetics />} />
+      <Route path="/skincare" element={<Skincare />} />
+      <Route path="/palette" element={<Palette />} />
+      <Route path="/shopping" element={<Shopping />} />
+      <Route path="/expiry" element={<Expiry />} />
+    </Routes>
   );
 }
 
