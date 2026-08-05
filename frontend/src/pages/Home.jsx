c7a894fab4df00423e8accdafc917d5c05dbe485
Sunny <cbe112030@nptu.edu.tw>
2026-08-05 15:09:54 +0800

    fixed Home title

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -100,12 +100,12 @@ function Home() {
       <div className="main">
 
         <Header
-          title="你的美妝助理"
+          title="我的美妝助理💅🏻"
           toggleMenu={toggleMenu}
         />
 
         <p className="subtitle">
-          你的美妝管理中心
+          我的美妝管理中心
         </p>
 
         <div className="info-grid">
